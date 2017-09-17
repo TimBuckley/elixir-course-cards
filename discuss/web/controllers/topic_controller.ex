f2baf255d201fdd897ee366c3542cd2648344bba
Tim Buckley <timothypbuckley@gmail.com>
2017-09-17 17:12:07 -0400

    Add create topic controller method

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -7,6 +7,23 @@ defmodule Discuss.TopicController do
     render conn, "new.html", changeset: changeset
   end
 
+  def create(conn, %{"topic" => topic} = params) do
+    insertion = %Topic{}
+    |> Topic.changeset(topic)
+    |> Repo.insert
+
+    case insertion do
+      {:ok, _post} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "Ya dun goofed")
+        |> redirect(to: topic_path(conn, :new))
+    end
+  end
+
   def index(conn, _params) do
     topics = Repo.all(Topic)
     render conn, "index.html", topics: topics
