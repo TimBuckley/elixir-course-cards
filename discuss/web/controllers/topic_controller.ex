91b5dbc117a87326d0eadc00259355e9ac22821b
Tim Buckley <timothypbuckley@gmail.com>
2017-09-17 17:11:36 -0400

    Add index controller method, view, route & template

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -2,8 +2,13 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
   alias Discuss.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render conn, "new.html", changeset: changeset
+  end
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
   end
 end
