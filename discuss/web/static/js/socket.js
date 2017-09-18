0e92ecd271755aedba09fb1ef9ce0e40f025ed2a
Tim Buckley <timothypbuckley@gmail.com>
2017-09-17 23:17:25 -0400

    Set up posting of comment along websockets

--- discuss/web/static/js/socket.js
+++ discuss/web/static/js/socket.js
@@ -4,14 +4,25 @@ let socket = new Socket('/socket', { params: { token: window.userToken } })
 
 socket.connect()
 
-let channel = socket.channel('topic:subtopic', {})
-channel
-  .join()
-  .receive('ok', resp => {
-    console.log('Joined successfully', resp)
-  })
-  .receive('error', resp => {
-    console.log('Unable to join', resp)
-  })
+window.location
 
-export default socket
+const createSocket = topicId => {
+  let channel = socket.channel(`comments:${topicId}`, {})
+  channel
+    .join()
+    .receive('ok', resp => {
+      console.log('Joined successfully', resp)
+    })
+    .receive('error', resp => {
+      console.log('Unable to join', resp)
+    })
+
+  document.querySelector('button').addEventListener('click', () => {
+    const content = document.querySelector('textarea').value
+    channel.push('comment:add', { content })
+  })
+}
+// window.mySocket = socket
+// window.myChannel = channel
+
+window.createSocket = createSocket
